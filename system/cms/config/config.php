05ef4de3a2a0ec698ff9418a23825fda16224081
anhduc89 <vaduc89@gmail.com>
2016-10-06 15:32:53 +0700

    sua loi

--- system/cms/config/config.php
+++ system/cms/config/config.php
@@ -26,7 +26,7 @@ $config['base_url']	= '';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
@@ -230,7 +230,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = 'T1gpXHd6UbJ703mElLwrjdPAAUlDX6Lx/+RvjTuOAi';
 
 /*
 |--------------------------------------------------------------------------
