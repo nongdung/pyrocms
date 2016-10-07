6a0462c18fd233672393c55064b9a73cbac2e16c
anhduc89 <vaduc89@gmail.com>
2016-10-07 14:20:01 +0700

    doi ten file model product_m. Viet cac ham show categories, products, comment, ham tim kiem product

--- addons/shared_addons/modules/products/controllers/products.php
+++ addons/shared_addons/modules/products/controllers/products.php
@@ -2,14 +2,46 @@
 
 class Products extends Public_Controller{
     
-    public function __construct(){
+    public function __construct()
+    {
         parent::__construct();
         $this->load->driver('Streams');
         $this->lang->load('products');
+        $this->load->model("products_m");
     }
     
-    public function index(){
-        echo '123';
+    # hiển thị categories
+    public function index()
+    {
+       $data = $this->products_m->get_categories();
+       
     }
+
+    # hiển thị products
+    public function get_products()
+    {
+    	$data = $this->products_m->get_products();
+        #print_r($data);
+    }
+
+    # hiển thị comments
+    public function get_comments()
+    {
+    	$data = $this->products_m->get_comments();
+        
+    }
+
+    # tìm kiếm
+    public function search()
+    {
+    	if($this->input->get())
+    	{
+    		$keyword 		 =  $this->input->get('keyword');
+    		$data['results'] =  $this->products_m->search($keyword);
+    	}
+    	print_r($data);
+    	#$this->load->view();
+    }
+   
 }
 
