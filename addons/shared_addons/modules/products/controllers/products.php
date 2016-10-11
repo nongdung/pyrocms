773a8ddb452694fb8ba94d2eab04957b9d4a9b30
anhduc89 <vaduc89@gmail.com>
2016-10-11 14:52:18 +0700

    update products.php

--- addons/shared_addons/modules/products/controllers/products.php
+++ addons/shared_addons/modules/products/controllers/products.php
@@ -13,14 +13,7 @@ class Products extends Public_Controller{
     # hiển thị categories
     public function index()
     {
-       #$data['results'] = $this->products_m->get_categories();    
-    }
-
-    # hiển thị products
-    public function getProducts()
-    {
-    	#$data = $this->products_m->get_products();
-        $params = array(
+       $params = array(
                 'stream'    => 'products',
                 'namespace' => 'products',
                 'order_by'  => 'ordering_count',
@@ -29,9 +22,9 @@ class Products extends Public_Controller{
         $data['products'] = $this->streams->entries->get_entries($params);
         $this->template
             ->title($this->module_details['name'])
-            ->build('design/categories',$data);    	       
+            ->build('design/categories',$data);    
     }
-
+    
     # hiển thị chi tiết sản phẩm 
     public function detail($id_p)
     {
