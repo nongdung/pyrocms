ddb886f3e494fe3186c8ed2e08557dceed5b2372
MinhNguyen <minhnq306@gmail.com>
2016-10-13 16:35:49 +0700

    replace old model with new model

--- addons/shared_addons/modules/products/models/products_m.php
+++ addons/shared_addons/modules/products/models/products_m.php
@@ -7,54 +7,25 @@ class Products_m extends MY_Model
         parent::__construct();
 
     }
-
-    # lấy categories
-    public function get_categories()
-    {
-    	$this->db->select("*");
-    	$this->db->from("products_categories");
-    	$query = $this->db->get();
-    	return $query->result();
+    //update server
+    public function ajaxlist($limit = 2, $offset = 0){
+        $this->db->select('*');
+        $this->db->from('products_products');
+        $this->db->join('files', 'files.id = products_products.p_image', 'inner');
+        $this->db->limit($limit, $offset);
+        $query = $this->db->get();
+        $data = $query->result_array();
+        foreach ($data as $index=>$value){
+            $data[$index]["path"] = str_replace("{{ url:site }}",BASE_URL,$value['path']);
+        }
+        return $data;
     }
 
-    # lấy products
-    public function get_products()
-    {
-    	$this->db->select("*");
-    	$this->db->from("products_products");
-    	$query = $this->db->get();
-    	return $query->result();
-    }
-    # lấy ra chi tiết sản phẩm
     public function detail_products($id_p)
     {
         $this->db->select("*");
-        $this->db->where("id",$id_p);
+        $this->db->where("products_products.id",$id_p);
         $this->db->from("products_products");
-        $query = $this->db->get();
-        return $query->row_array();
-    }
-    # lấy comments
-    public function get_comments()
-    {
-    	$this->db->select("*");
-    	$this->db->from("products_comment");
-    	$query = $this->db->get();
-    	return $query->result();
-    }
-
-    # tìm kiếm theo products
-    public function search($keyword)
-    {
-	    $this->db->like('p_name',$keyword);
-	    $this->db->or_like('p_id',$keyword);
-	    $query = $this->db->get('products_products');
-	    return $query->result();
-    }
-
-    public function server(){
-        $this->db->select('*');
-        $this->db->from('products_products');
         $this->db->join('files', 'files.id = products_products.p_image', 'inner');
         $query = $this->db->get();
         $data = $query->result_array();
