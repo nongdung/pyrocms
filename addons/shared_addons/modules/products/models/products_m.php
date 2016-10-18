35077e509c19c6873e4119b7632db8b0480462f5
anhduc89 <vaduc89@gmail.com>
2016-10-18 17:11:41 +0700

    update products_m

--- addons/shared_addons/modules/products/models/products_m.php
+++ addons/shared_addons/modules/products/models/products_m.php
@@ -9,9 +9,9 @@ class Products_m extends MY_Model
     }
     //update server
     public function ajaxlist($limit = 2, $offset = 0){
-        $this->db->select('*');
+        $this->db->select("*");
         $this->db->from('products_products');
-        $this->db->join('files', 'files.id = products_products.p_image', 'inner');
+        $this->db->join('files', 'files.id = products_products.p_image','inner');
         $this->db->limit($limit, $offset);
         $query = $this->db->get();
         $data = $query->result_array();
@@ -22,16 +22,45 @@ class Products_m extends MY_Model
     }
 
     public function detail_products($id_p)
-    {
-        $this->db->select("*");
+    {            
+        $this->db->select("products_products.id"); 
+        $this->db->select("products_products.p_id");
+        $this->db->select("products_products.p_name");
+        $this->db->select("products_products.p_price");
+        $this->db->select("products_products.p_discount");
+        $this->db->select("products_products.p_long_description");
+        $this->db->select("products_products.category_id");
+        $this->db->select("files.path");
+        $this->db->select("products_comment.comments"); 
+        $this->db->select("products_comment.user_id_c"); 
+        $this->db->select("products_comment.reply_id");
+        $this->db->select("products_products.p_image");
+        
         $this->db->where("products_products.id",$id_p);
         $this->db->from("products_products");
         $this->db->join('files', 'files.id = products_products.p_image', 'inner');
+        $this->db->join('products_comment', 'products_comment.product_id_c = products_products.id', 'inner');
         $query = $this->db->get();
         $data = $query->result_array();
         foreach ($data as $index=>$value){
             $data[$index]["path"] = str_replace("{{ url:site }}",BASE_URL,$value['path']);
         }
         return $data;
+    }
+
+    public function comments($id_p)
+    {
+        $this->db->select("*");
+        $this->db->where("products_products.id",$id_p);
+        $this->db->from("products_products");
+        $this->db->join('products_comment', 'products_comment.product_id_c = products_products.id', 'inner');
+        $query = $this->db->get();
+        return $query->result_array();
+    }
+
+    public function insert_comments($data)
+    {
+        $this->db->insert("products_comment",$data);
+        return true;
     }
 }
