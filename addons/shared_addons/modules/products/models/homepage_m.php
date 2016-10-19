3ebf69496d5de151077c1d99d104a9e6931a8975
MinhNguyen <minhnq306@gmail.com>
2016-10-19 10:36:45 +0700

    update function ajaxlist to handle new query order by line 17

--- addons/shared_addons/modules/products/models/homepage_m.php
+++ addons/shared_addons/modules/products/models/homepage_m.php
@@ -14,16 +14,22 @@ class Homepage_m extends MY_Model
         return $query->result();
     }
     //update server
-    public function ajaxlist($limit = 2, $offset = 0,$cat=null){
+    public function ajaxlist($limit = 2, $offset = 0,$cat=null, $f_id = null){
         $this->db->select('products_products.id,p_id,p_name,p_image,p_price,p_highlight,p_discount,p_unit,p_short_description,p_long_description,category_id,files.id as f_id,files.path,products_categories.c_name');
         if($cat!=""){
         $this->db->where('category_id',$cat);
-        }
+        };
         $this->db->from('products_products');
         $this->db->join('files', 'files.id = products_products.p_image');
         $this->db->join('products_categories','products_categories.id = products_products.category_id');
         $this->db->limit($limit, $offset);
-        
+        if($f_id=="1"){
+             $this->db->order_by('p_price','DESC');
+        }
+        if($f_id=="2"){
+             
+             $this->db->order_by('p_price','ASC');
+        }
         $query = $this->db->get();
         $data = $query->result_array();
         foreach ($data as $index=>$value){
