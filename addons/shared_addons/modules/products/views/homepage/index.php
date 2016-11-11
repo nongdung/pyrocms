191bc8e0824ab41e91204686370817b37355b492
MinhNguyen <minhnq306@gmail.com>
2016-11-11 13:55:44 +0700

    add loading picture

--- addons/shared_addons/modules/products/views/homepage/index.php
+++ addons/shared_addons/modules/products/views/homepage/index.php
@@ -1,9 +1,13 @@
+
 <div class="jumbotron">
   <div class="container text-center">
     <h1>Main Menu</h1>
   </div>
 </div>
-
+<div class="preload">
+    <img src="addons/shared_addons/modules/products/img/loading_spinner.gif" />
+</div> 
+ 
 <div id="product"></div>
 <script type="text/babel" src="<?php echo base_url("addons/shared_addons/modules/products")?>/views/homepage/main.js"></script>
     
