0397fa9e2b2dbee66ad6965f20f2aac58465b2b0
MinhNguyen <minhnq306@gmail.com>
2017-01-03 10:43:29 +0700

    update nav-bả

--- addons/shared_addons/modules/products/views/homepage/index.php
+++ addons/shared_addons/modules/products/views/homepage/index.php
@@ -1,14 +1,33 @@
-
 <div class="jumbotron">
   <div class="container text-center">
     <h1>Main Menu</h1>
+    
   </div>
 </div>
-<div class="preload">
+<div class="preload ">
     <img src="addons/shared_addons/modules/products/img/loading_spinner.gif" />
 </div> 
  
+
+
 <div id="product"></div>
 <script type="text/babel" src="<?php echo base_url("addons/shared_addons/modules/products")?>/views/homepage/main.js"></script>
-    
-  
+
+
+<nav class="asd navbar-inverse navbar-fixed-bottom">
+
+    <div class="container">
+        <ul class="nav navbar-nav navbar-right mobile-bar">
+            <li class="homebutton">
+                <a href="<?php echo base_url()?>products"><span class="glyphicon glyphicon-home" aria-hidden="true"></span></a>
+            </li>
+            <li class="searchbutton">
+                <a href="<?php echo base_url()?>products/search"><span class="glyphicon glyphicon-search" aria-hidden="true"></span></a>
+            </li>
+            
+        </ul>
+    </div>
+</nav>
+
+
+
