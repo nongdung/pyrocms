a43dee9d583e67d8b1ae1b56fd660e2befed7f0f
MinhNguyen <minhnq306@gmail.com>
2017-01-03 10:15:37 +0700

    update config file for dynamodb connecting

---
+++ addons/shared_addons/modules/products/config/aws.inc.php
@@ -0,0 +1,8 @@
+<?php 
+require 'E:\Work\xampp\htdocs\pyrocms-pyrocms\addons\shared_addons\modules\products\libraries\aws-autoloader.php';
+use Aws\DynamoDb\Exception\DynamoDbException;
+use Aws\DynamoDb\Marshaler;
+use Aws\DynamoDb\DynamoDbClient;
+use Aws\Credentials\CredentialProvider;
+
+?>
