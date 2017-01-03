fc0fd027f83c827f7d248e75e94a8ac9eb577ae3
MinhNguyen <minhnq306@gmail.com>
2017-01-03 10:16:34 +0700

    update controller

---
+++ addons/shared_addons/modules/products/controllers/aws.php
@@ -0,0 +1,6 @@
+<?php 
+require 'E:\Work\xampp\htdocs\pyrocms-pyrocms\addons\shared_addons\modules\products\libraries\aws-autoloader.php';
+use Aws\DynamoDb\Exception\DynamoDbException;
+use Aws\DynamoDb\Marshaler;
+use Aws\DynamoDb\DynamoDbClient;
+?>
